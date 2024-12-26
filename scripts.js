2542674d41f40149325baaf2107a5ca7bea1d611
ahmed-au8 <ubdullad@mail.uc.edu>
2024-12-26 18:55:35 -0500

    added popup functionality

--- scripts.js
+++ scripts.js
@@ -1,23 +1,4 @@
-// Function to open modal with specific script information
-function openModal(title, description, downloadLink) {
-    document.getElementById("scriptTitle").innerText = title;
-    document.getElementById("scriptDescription").innerText = description;
-    document.getElementById("downloadButton").onclick = function() {
-        window.location.href = downloadLink;
-    };
-
-    document.getElementById("scriptModal").style.display = "flex";
-}
-
-// Function to close modal
-function closeModal() {
-    document.getElementById("scriptModal").style.display = "none";
-}
-
-// Close the modal when clicking outside of it
-window.onclick = function(event) {
-    const modal = document.getElementById("scriptModal");
-    if (event.target == modal) {
-        closeModal();
-    }
+function togglePopup() {
+    const popup = document.getElementById('popup');
+    popup.style.display = popup.style.display === 'block' ? 'none' : 'block';
 }
